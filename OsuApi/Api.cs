4220560f985a8a4cd98aa4ed4efd00bb0a62bacf
Jackson Saylor <jackson.saylor@yahoo.com>
2017-05-17 16:40:47 -0400

    Fixed earlier push

--- OsuApi/Api.cs
+++ OsuApi/Api.cs
@@ -10,6 +10,7 @@ namespace OsuApi
         public IScoreSpecificQuery GetScores => new ScoreQuery(ApiKey);
         public IBeatmapSpecificQuery GetSpecificBeatmap => new BeatmapQuery(ApiKey);
         public IUserSpecificQuery GetUser => new UserQuery(ApiKey);
+        public IBestPlayQuery GetBestPlay => new BestPlayQuery(ApiKey);
 
         public Api(string apiKey) => ApiKey = apiKey;
     }
